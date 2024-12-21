8ff2ac103a018cd42f89a567413bdca20a692a9b
이동희 <old606900@outlook.com>
2024-12-21 10:28:56 +0900

    Last modifying

--- A.java
+++ A.java
@@ -1,10 +1,6 @@
 public class A {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD!");
-<<<<<<< HEAD
         System.out.println("TESTING DEV MAIN");
-=======
-        System.out.println("TESTING ON DEV C");
->>>>>>> Dev-C
     }
 }
